3db2982d8a7dfb75ff486b3a92d3e6e9421ad57f
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-09 15:58:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,9 @@ def oxford_comma(array)
     array.join
   elsif array.size == 2
     array.join(" and ")
-  else array.size == 3
+  else array.size >= 3
     array.join (", ")
-    array << and
-  end
+    
+    
+    
 end
