b7bd33431623b23a675ced97a525548b0222be45
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-09 15:48:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if Array.size <= 1
+    Array.join
+  elsif Array.size == 2
+    Array.join("and")
+  else Array.size >= 3
+    Array.join (",")
+  end
+    
+    
 end
